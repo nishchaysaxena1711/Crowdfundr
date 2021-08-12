c1f4e931c099d03c536f50f36a35d25b6a1e4dbb
Nishchay Saxena <nishchay.saxena@appdirect.com>
2021-08-12 17:57:56 +0530

    added test for credit function

--- test/crowdfundr.js
+++ test/crowdfundr.js
@@ -7,7 +7,7 @@ describe("Crowdfundr", function() {
     beforeEach(async function () {
         // Get the ContractFactory and Signers here.
         CrowdFundr = await ethers.getContractFactory("Crowdfundr");
-        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        [owner, depositor1, depositor2, ...depositors] = await ethers.getSigners();
 
         crowdfundr = await CrowdFundr.connect(owner).deploy("CrowdFundr Test Project", BigNumber.from(100), { value: 50 });
         await crowdfundr.deployed();
@@ -20,5 +20,25 @@ describe("Crowdfundr", function() {
         expect(await crowdfundr.getProjectMaximumBalance()).to.deep.equal(BigNumber.from(100));
         expect(await crowdfundr.getAddresses()).to.deep.equal([owner.address]);
         expect(await crowdfundr.getBalanceOfDepositor(owner.address)).to.deep.equal(BigNumber.from(50));
+        expect(await crowdfundr.getBalanceOfDepositor(depositor1.address)).to.deep.equal(BigNumber.from(0));
+    });
+
+    it("validate balance is credited in project balance or not", async function() {
+        expect(await crowdfundr.getProjectBalance()).to.deep.equal(BigNumber.from(50));
+        expect(await crowdfundr.getProjectMaximumBalance()).to.deep.equal(BigNumber.from(100));
+
+        let tx = await crowdfundr.connect(depositor1).credit({ value: 5 });
+
+        expect(await crowdfundr.getProjectBalance()).to.deep.equal(BigNumber.from(55));
+        expect(await crowdfundr.getBalanceOfDepositor(owner.address)).to.deep.equal(BigNumber.from(50));
+        expect(await crowdfundr.getBalanceOfDepositor(depositor1.address)).to.deep.equal(BigNumber.from(5));
+        expect(await crowdfundr.getBalanceOfDepositor(depositor2.address)).to.deep.equal(BigNumber.from(0));
+
+        tx = await crowdfundr.connect(depositor2).credit({ value: 10 });
+
+        expect(await crowdfundr.getProjectBalance()).to.deep.equal(BigNumber.from(65));
+        expect(await crowdfundr.getBalanceOfDepositor(owner.address)).to.deep.equal(BigNumber.from(50));
+        expect(await crowdfundr.getBalanceOfDepositor(depositor1.address)).to.deep.equal(BigNumber.from(5));
+        expect(await crowdfundr.getBalanceOfDepositor(depositor2.address)).to.deep.equal(BigNumber.from(10));
     });
 });
